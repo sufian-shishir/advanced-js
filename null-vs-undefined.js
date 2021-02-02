b7458d33d3e331530d2383399e2be539b59a61aa
sufian shishir <abusufian.shishir2015@gmail.com>
2021-02-02 19:04:18 +0600

    null-vs-undefined examples

---
+++ null-vs-undefined.js
@@ -0,0 +1,33 @@
+// // let pakhi;
+// console.log(pakhi);
+
+
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// const result = add(13, 82);
+// console.log(result);
+
+
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+
+
+
+// const premik = {name:"smart dude", phone:01010101010};
+// console.log(premik.gf);
+
+
+
+// let fun = undefined;
+// console.log(fun);
+
+
+// let ages = [2, 5, 9];
+// console.log(ages[11]);
