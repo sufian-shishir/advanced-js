55275d7ca00419541dd2fa2f612e837da4087050
sufian shishir <abusufian.shishir2015@gmail.com>
2021-02-02 20:28:07 +0600

    easy but abar dekhbo

---
+++ scope.js
@@ -0,0 +1,11 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    return result;
+}
+const output = sum(3, 7);
+console.log(bonus);
+console.log(output);
+
+// must see the video another tym(23-7);
