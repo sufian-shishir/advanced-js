90e729155139a5b986787cc5b302cb98824b2d27
sufian shishir <abusufian.shishir2015@gmail.com>
2021-02-02 21:14:13 +0600

    bujhar bepar ase

---
+++ array-split.js
@@ -0,0 +1,18 @@
+// slice = (2, 5)= position 2 theke 5 positioner ag porjonto delete;
+// splice = (2, 5)= position 2 theke 5 ta delete hobe ;
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = numbers.slice(2, 5);
+// console.log(part);
+// console.log(numbers);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// const removed = numbers.splice(2, 3);
+// console.log(removed);
+// console.log(numbers);
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const together = numbers.join(',');
+console.log(together);
