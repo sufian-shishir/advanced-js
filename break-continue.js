61c6120b899774f0309f41582aeea290f20054e6
sufian shishir <abusufian.shishir2015@gmail.com>
2021-02-03 21:22:28 +0600

    hii

---
+++ break-continue.js
@@ -0,0 +1,18 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i = 0; i < numbers.length; i++) {
+   
+//     if (numbers[i] > 5) {
+//         break;
+//     }
+//     console.log(numbers[i]);
+// }
+
+
+const numbers = [1, -2, 3, 4, -5, -6, 7, -8, 9];
+for (let i = 0; i < numbers.length; i++) {
+   
+    if (numbers[i] < 0) {
+        continue;
+    }
+    console.log(numbers[i]);
+}
